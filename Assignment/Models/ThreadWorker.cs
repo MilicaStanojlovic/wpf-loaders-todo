b0bb86254edf2f465cf08320ecdad4a1e017a7f2
Milica Stanojlovic <mstanojlovic222@gmail.com>
2026-05-25 11:11:03 +0200

    feat: add ThreadWorker model with Progress, IsActive, Cancel

--- Assignment/Models/ThreadWorker.cs
+++ Assignment/Models/ThreadWorker.cs
@@ -1,12 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 
 namespace Assignment.Models
 {
-    class ThreadWorker
+    public class ThreadWorker : INotifyPropertyChanged
     {
+        private int _elapsed;
+
+        public int Duration { get; set; }
+
+        public int Elapsed
+        {
+            get => _elapsed;
+            set
+            {
+                _elapsed = value;
+                OnPropertyChanged(nameof(Elapsed));
+                OnPropertyChanged(nameof(Progress));
+            }
+        }
+
+        public double Progress => Duration > 0
+            ? (double)Elapsed / Duration * 100.0
+            : 0;
+
+        public bool IsActive { get; private set; } = true;
+
+        public void Cancel()
+        {
+            IsActive = false;
+            OnPropertyChanged(nameof(IsActive));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
