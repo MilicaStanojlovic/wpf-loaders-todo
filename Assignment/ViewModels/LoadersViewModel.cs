434b276e4f0463a84a2454c1773e1aee396564b3
Milica Stanojlovic <mstanojlovic222@gmail.com>
2026-05-25 10:52:24 +0200

    feat: implement LoadersViewModel with Threads and TotalProgress

--- Assignment/ViewModels/LoadersViewModel.cs
+++ Assignment/ViewModels/LoadersViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment.Commands;
+using Assignment.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,44 @@ namespace Assignment.ViewModels
 {
     public class LoadersViewModel : ViewModelBase
     {
+        public IList<ThreadWorker> Threads { get; private set; }
 
+        public double TotalProgress
+        {
+            get
+            {
+                var active = Threads.Where(t => t.IsActive).ToList();
+                if (!active.Any()) return 0.0;
+                return active.Average(t => t.Progress);
+            }
+        }
+
+        public LoadersViewModel()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            var rng = new Random();
+            Threads = new List<ThreadWorker>
+            {
+                new ThreadWorker { Duration = rng.Next(10, 51) },
+                new ThreadWorker { Duration = rng.Next(10, 51) },
+                new ThreadWorker { Duration = rng.Next(10, 51) }
+            };
+
+            foreach (var thread in Threads)
+            {
+                thread.PropertyChanged += (s, e) =>
+                {
+                    if (e.PropertyName == nameof(ThreadWorker.Progress) ||
+                        e.PropertyName == nameof(ThreadWorker.IsActive))
+                    {
+                        OnPropertyChanged(nameof(TotalProgress));
+                    }
+                };
+            }
+        }
     }
 }
